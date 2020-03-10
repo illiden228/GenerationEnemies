fe6b2ddb120f4b6c9ab68defc0bc9eb17556d37c
illidenium <den.khabibullin@gmail.com>
2020-03-10 17:05:52 +0300

    Spawn Zombies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Transform _target;
+    private Vector3 _targetDirection;
+
+    private void Start()
+    {
+        SetTarget(FindObjectOfType<Player>());
+    }
+
+    private void Update()
+    {
+        if(_target == null)
+        {
+            return;
+        }
+
+        transform.LookAt(_target, transform.up);
+        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, _speed * Time.deltaTime);
+    }
+
+    public void SetTarget(Player target)
+    {
+        _target = target.transform;
+        _targetDirection = new Vector3(_target.position.x, transform.position.y, _target.position.z);
+    }
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float _delay;
+    [SerializeField] private GameObject _enemyExample;
+    [SerializeField] private Text _timeText;
+    private float _lastSpawn;
+    private Transform[] _spawnDots;
+    
+    void Start()
+    {
+        _spawnDots = GetComponentsInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        _timeText.text = Mathf.Ceil(Time.time).ToString();
+        if (_lastSpawn >= _delay)
+        {
+            foreach(Transform spawnDot in _spawnDots)
+            {
+                Instantiate(_enemyExample, spawnDot);
+            }
+            _lastSpawn = 0;
+        }
+        _lastSpawn += Time.deltaTime;
+    }
+}
